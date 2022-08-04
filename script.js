43b6ee2909eda12f76ecdc7c8e9f82cd22cc1b4e
Cristian Alegre Bustos <CNAlegreBustos@teco.com.ar>
2022-08-04 14:11:58 -0300

    Crear Eventos para descargar la app

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ const googleButton=document.getElementById("GoogleButton");
 const linkToPlayStore="https://play.google.com/store/apps/details?id=ar.com.personalpay&referrer=utm_source%3DPePa%26utm_medium%3Dreferral%26utm_term%3Dhome-BanHer%26utm_content%3DDesc_Andr-plata_personal%26utm_campaign%3DRF-Ecosistema";
 const linkToAppStore="https://apps.apple.com/app/apple-store/id1548817439?pt=122556632&ct=RF-Ecosistema-PePa-Banner-Home&mt=8";
 
-googleButton.addEventListener("touchstart", ev =>{
-    location.reload(linkToPlayStore)
-})
+googleButton.addEventListener("click", dowloadApp(linkToAppStore))
 
-googleButton.addEventListener("touchstart", ev =>{
-    location.reload(linkToAppStore)
-})
+googleButton.addEventListener("click", dowloadApp(linkToAppStore));
+
+const dowloadApp= (link)=>  location.reload(link)

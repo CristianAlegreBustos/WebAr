e329b5ffa61bede4b4c33c6190914d815434a364
Cristian Alegre Bustos <CNAlegreBustos@teco.com.ar>
2022-08-04 16:20:02 -0300

    Fix script links

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const appleButton=document.getElementById("AppleButton");
-const googleButton=document.getElementById("GoogleButton");
-const linkToPlayStore="https://play.google.com/store/apps/details?id=ar.com.personalpay&referrer=utm_source%3DPePa%26utm_medium%3Dreferral%26utm_term%3Dhome-BanHer%26utm_content%3DDesc_Andr-plata_personal%26utm_campaign%3DRF-Ecosistema";
-const linkToAppStore="https://apps.apple.com/app/apple-store/id1548817439?pt=122556632&ct=RF-Ecosistema-PePa-Banner-Home&mt=8";
-
-googleButton.addEventListener("click", dowloadApp(linkToAppStore))
+const appleButton = document.getElementById("AppleButton");
+const googleButton = document.getElementById("GoogleButton");
+const linkToPlayStore =
+  "https://play.google.com/store/apps/details?id=ar.com.personalpay&referrer=utm_source%3DPePa%26utm_medium%3Dreferral%26utm_term%3Dhome-BanHer%26utm_content%3DDesc_Andr-plata_personal%26utm_campaign%3DRF-Ecosistema";
+const linkToAppStore =
+  "https://apps.apple.com/app/apple-store/id1548817439?pt=122556632&ct=RF-Ecosistema-PePa-Banner-Home&mt=8";
 
 googleButton.addEventListener("click", dowloadApp(linkToAppStore));
 
-const dowloadApp= (link)=>  location.reload(link)
+googleButton.addEventListener("click", dowloadApp(linkToAppStore));
 
-console.log(hola)
+const dowloadApp = (link) => location.reload(link);
+

d7a64ac2873c45a19325ba4dbea652c88986111a
Cristian Alegre Bustos <CNAlegreBustos@teco.com.ar>
2022-08-08 15:26:11 -0300

    Haciendo Imagenes Interactivas

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-
 document.addEventListener("DOMContentLoaded", function() {
-  const appleButton = document.gquerySelector("#AppleButton_plane");
+  const appleButton = document.querySelector("#AppleButton_plane");
   const googleButton = document.querySelector("#GoogleButton_plane");
   const linkToPlayStore =
     "https://play.google.com/store/apps/details?id=ar.com.personalpay&referrer=utm_source%3DPePa%26utm_medium%3Dreferral%26utm_term%3Dhome-BanHer%26utm_content%3DDesc_Andr-plata_personal%26utm_campaign%3DRF-Ecosistema";
   const linkToAppStore =
     "https://apps.apple.com/app/apple-store/id1548817439?pt=122556632&ct=RF-Ecosistema-PePa-Banner-Home&mt=8";
 
-  googleButton.addEventListener("click", e=>{console.log("Google click")});
+  googleButton.addEventListener("click", e=>{dowloadApp(linkToPlayStore)});
 
-  appleButton.addEventListener("click", e=>{console.log("Apple click")});
+  appleButton.addEventListener("click", e=>{dowloadApp(linkToAppStore)});
 
- // const dowloadApp = (link) => location.reload(link);
+  const dowloadApp = (link) => window.open(link);
 
 });
